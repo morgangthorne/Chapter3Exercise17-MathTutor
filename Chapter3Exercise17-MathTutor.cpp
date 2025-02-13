0d336d10d287857306dccbe571fb419ee6609a0c
Morgan Thorne <mt34452@email.vccs.edu>
2025-02-13 14:53:04 -0500

    Allowed user to enter an answer and program to display whether the answer is right or wrong

--- Chapter3Exercise17-MathTutor.cpp
+++ Chapter3Exercise17-MathTutor.cpp
@@ -7,6 +7,10 @@ Write program that can be used as a math tutor
 1: Program should display two random numbers to be added
 2: Program should pause while students works on problem
 3: When student is ready they can press a key and the program will show correct answer
+
+Changes:
+1:Allow user to enter an answer
+2:Display correct answer and tell student if they are right or wrong
 */
 
 #include <iostream>
@@ -16,21 +20,24 @@ using namespace std;
 
 int main()
 {
-   //Variables for the equation
+    //Variables for the equation
     int x = 247;
     int y = 129;
-    int sum = (x + y);
+    int sum = 376;
+    int usersum;
 
     //Output for the question
-    cout << "Solve the equation below:" << endl;
-    cout << setw(10) << x << endl << setw(7) << "+" << y << endl << "Press Enter to check your answer: ";
-    
-    //Allows user to enter anykey they want to check their answer
-    cin.ignore();
+    cout << "Solve the equation below, enter the answer and press Enter to Check Work:" << endl;
+    cout << setw(10) << x << endl << setw(7) << "+" << y << endl;
+    cout << setw(10) << "---" << endl;
+    cout << setw(7) << "";
+    cin >> usersum;
 
-    //Equation with answer
-    cout << setw(10) << x << endl << setw(7) << "+" << y << endl << setw(10) << "---" << endl << setw(10) << sum << endl;
+    if (usersum != sum)
+        cout << "You are incorrect, the correct answer is " << sum;
+
+    else if (usersum = sum)
+        cout << "You are correct, the answer was " << sum;
 
 }
-
 
